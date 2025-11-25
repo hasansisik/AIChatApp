d018686b441ddd45abee438341d36a32b36f3559
Hasan Şişik <sisik.hsn@gmail.com>
2025-11-25 19:07:48 +0300

    config

--- config.ts
+++ config.ts
@@ -1,3 +1,33 @@
-export const server = "http://16.171.161.33:5001/v1";
+import Constants from "expo-constants";
+
+const DEFAULT_API_HOST = "16.171.161.33";
+const DEFAULT_API_PORT = "5001";
+const DEFAULT_API_PATH = "/v1";
+
+const resolveServerBaseURL = () => {
+  if (process.env.EXPO_PUBLIC_API_URL) {
+    return process.env.EXPO_PUBLIC_API_URL;
+  }
+
+  const hostUri =
+    Constants?.expoConfig?.hostUri ??
+    Constants?.manifest2?.extra?.expoGo?.developerServerOrigin ??
+    Constants?.manifest?.debuggerHost;
+
+  if (!hostUri) {
+    return `http://${DEFAULT_API_HOST}:${DEFAULT_API_PORT}${DEFAULT_API_PATH}`;
+  }
+
+  const host = hostUri.split(":")[0];
+
+  // Expo Go içerisinde geliştirici makine IP'sini kullanmak istiyorsak
+  if (host && host !== "127.0.0.1" && host !== "localhost") {
+    return `http://${host}:${DEFAULT_API_PORT}${DEFAULT_API_PATH}`;
+  }
+
+  return `http://${DEFAULT_API_HOST}:${DEFAULT_API_PORT}${DEFAULT_API_PATH}`;
+};
+
+export const server = resolveServerBaseURL();
 export const aiServer = "http://16.171.161.33:8080/";
 export const aiPassword = "JSD876+J?*#Fd";
