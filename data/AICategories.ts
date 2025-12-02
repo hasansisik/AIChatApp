2999f92befd07701d2b387dfdf75b2721138a68c
Hasan Şişik <sisik.hsn@gmail.com>
2025-12-02 17:58:41 +0300

    servicejson

--- data/AICategories.ts
+++ data/AICategories.ts
@@ -36,8 +36,8 @@ export const aiCategories: AICategory[] = [
     description: 'ai.descriptions.emma',
     avatar_id: 1,
     voice: 'alloy', // Kadın sesi
-    video: require('@/assets/video/aiexample.mp4'),
-    videoTTS: require('@/assets/video/aiexample1.mp4'),
+    video: require('@/assets/video/id1-s.mp4'),
+    videoTTS: require('@/assets/video/id1-k.mp4'),
   },
   {
     id: '2',
@@ -48,8 +48,8 @@ export const aiCategories: AICategory[] = [
     description: 'ai.descriptions.alexander',
     avatar_id: 2,
     voice: 'ash', // Erkek sesi
-    video: require('@/assets/video/aiexample.mp4'),
-    videoTTS: require('@/assets/video/aiexample1.mp4'),
+    video: require('@/assets/video/id1-s.mp4'),
+    videoTTS: require('@/assets/video/id1-k.mp4'),
   },
   {
     id: '3',
@@ -60,8 +60,8 @@ export const aiCategories: AICategory[] = [
     description: 'ai.descriptions.sophia',
     avatar_id: 3,
     voice: 'shimmer', // Kadın sesi
-    video: require('@/assets/video/aiexample.mp4'),
-    videoTTS: require('@/assets/video/aiexample1.mp4'),
+    video: require('@/assets/video/id1-s.mp4'),
+    videoTTS: require('@/assets/video/id1-k.mp4'),
   },
   {
     id: '4',
@@ -72,8 +72,8 @@ export const aiCategories: AICategory[] = [
     description: 'ai.descriptions.james',
     avatar_id: 4,
     voice: 'onyx', // Erkek sesi
-    video: require('@/assets/video/aiexample.mp4'),
-    videoTTS: require('@/assets/video/aiexample1.mp4'),
+    video: require('@/assets/video/id1-s.mp4'),
+    videoTTS: require('@/assets/video/id1-k.mp4'),
   },
   {
     id: '5',
@@ -84,8 +84,8 @@ export const aiCategories: AICategory[] = [
     description: 'ai.descriptions.isabella',
     avatar_id: 5,
     voice: 'shimmer', // Kadın sesi
-    video: require('@/assets/video/aiexample.mp4'),
-    videoTTS: require('@/assets/video/aiexample1.mp4'),
+    video: require('@/assets/video/id1-s.mp4'),
+    videoTTS: require('@/assets/video/id1-k.mp4'),
   },
   {
     id: '6',
@@ -96,7 +96,7 @@ export const aiCategories: AICategory[] = [
     description: 'ai.descriptions.michael',
     avatar_id: 6,
     voice: 'onyx', // Erkek sesi
-    video: require('@/assets/video/aiexample.mp4'),
-    videoTTS: require('@/assets/video/aiexample1.mp4'),
+    video: require('@/assets/video/id1-s.mp4'),
+    videoTTS: require('@/assets/video/id1-k.mp4'),
   },
 ];
